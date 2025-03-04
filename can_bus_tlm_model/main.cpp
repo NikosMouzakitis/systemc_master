ec178f4cb179028f7744f7ba037424f7335fca1f
NikosMouzakitis <mzktsn@gmail.com>
2025-03-04 08:10:57 +0200

    implementing event mechanism for bus arbitration
    Signed-off-by: NikosMouzakitis <mzktsn@gmail.com>

--- can_bus_tlm_model/main.cpp
+++ can_bus_tlm_model/main.cpp
@@ -17,9 +17,8 @@ struct CAN_message
 // can_node module.
 SC_MODULE(can_node)
 {
-	// initiator socket for transmission on CAN bus.
-	simple_initiator_socket<can_node> tx_socket;
-	
+	simple_initiator_socket<can_node> tx_socket; // initiator socket for transmission on CAN Bus.
+//	simple_target_socket<can_node> rx_socket;    // target socket for receiving messages from the CAN Bus
 	int id;
 
 	SC_CTOR(can_node) : tx_socket("tx_socket")
@@ -45,7 +44,7 @@ SC_MODULE(can_node)
 				payload.set_data_ptr(reinterpret_cast<unsigned char*>(&m));
 				payload.set_data_length(sizeof(CAN_message));
 				// Send the payload
-				cout << name() << " TX id = " << std::hex << m.id << " data:" << m.data <<  " at time " << sc_time_stamp() << endl;
+				cout << "@" << sc_time_stamp() << " " << name() << " TX id = " << std::hex << m.id << " data:" << m.data << endl;
 				tx_socket->b_transport(payload, delay);
 				wait(sc_time(20,SC_NS));
 			} else if(strcmp(name(),"top.node2") == 0)
@@ -57,7 +56,7 @@ SC_MODULE(can_node)
 				payload.set_data_ptr(reinterpret_cast<unsigned char*>(&m));
 				payload.set_data_length(sizeof(CAN_message));
 				// Send the payload
-				cout << name() << " TX id = " << std::hex << m.id << " data:" << m.data <<  " at time " << sc_time_stamp() << endl;
+				cout << "@" << sc_time_stamp() << " " << name() << " TX id = " << std::hex << m.id << " data:" << m.data << endl;
 				tx_socket->b_transport(payload, delay);
 				wait(sc_time(30,SC_NS));
 			} 
@@ -68,8 +67,13 @@ SC_MODULE(can_node)
 // can_bus module.
 SC_MODULE(can_bus)
 {
-	// initiator socket for transmission on CAN bus.
+	//simple_initiator_socket<can_bus> tx_soc	
+	// simple target socket for transmission on CAN bus.
 	simple_target_socket<can_bus> rx_socket[nodes];
+	
+	sc_event round_finished; //event to process received msgs.
+	std::vector<CAN_message> received_messages;
+	int pending_transfers = 0;
 
 	SC_CTOR(can_bus) 
 	{
@@ -80,16 +84,44 @@ SC_MODULE(can_bus)
 			// Register b_transport callback
 			rx_socket[i].register_b_transport(this, &can_bus::b_transport);
 		}	
+		SC_METHOD(process_messages);
+		sensitive << round_finished;
+		dont_initialize(); //to be triggered only by an event.
+		
 	}
 
 	//override b_transport()  
 	void b_transport(tlm_generic_payload& payload, sc_time& delay) {
 		// Read the data
 		CAN_message data = *reinterpret_cast<CAN_message*>(payload.get_data_ptr());
-		//unsigned int data = *reinterpret_cast<unsigned int*>(payload.get_data_ptr());
-		cout << name() << "[BUS reception]  id: " << std::hex << data.id << " data: " <<data.data << " at time " << sc_time_stamp() << endl;
-	}
+		//cout << name() << "[BUS reception]  id: " << std::hex << data.id << " data: " <<data.data << " at time " << sc_time_stamp() << endl;
+		pending_transfers++;
+		received_messages.push_back(data);
+		
+		//restart the timer for processing msgs.
+		round_finished.notify(1,SC_NS);
 
+	}
+	//processing received messages and performing arbitration if needed if collisions occur
+	//by simultaneous accesses.	
+	void process_messages(void) {
+
+		if(pending_transfers == 0) 
+			return ; // no message received.
+		int win_id = 0xFFFF;
+		for(const auto& msg: received_messages) {
+			if(msg.id < win_id)
+				win_id = msg.id;
+		}
+		
+		for(const auto& msg: received_messages) {
+			if(msg.id == win_id)
+				cout << name() << "[BUS reception]  id: " << std::hex << msg.id << " data: " <<msg.data << " at time " << sc_time_stamp() << endl;
+		}
+
+		received_messages.clear(); //clear the buffer for next reception.
+		pending_transfers = 0;
+	}
 };
 
 // Top Module
